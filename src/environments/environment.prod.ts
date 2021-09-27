d1ea9498dd07db2354f062986cf3ef9da49999fe
Mohammed Tabbakh <42747616+mohammedtabbakh@users.noreply.github.com>
2021-09-27 18:43:38 +0300

    add missing bracket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-firebaseConfig :{
+   firebaseConfig :{
     apiKey: "AIzaSyCXLz0TwWgsRf_F_kc8U3fnvkKgrSyTuD0",
     authDomain: "notely-694f0.firebaseapp.com",
     projectId: "notely-694f0",
@@ -8,4 +8,5 @@ firebaseConfig :{
     messagingSenderId: "87117305022",
     appId: "1:87117305022:web:2499bc8eabf04bcc8afaef",
     measurementId: "G-CB4JR2X7XW"
+  }
 };
