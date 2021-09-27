56b76b02df121e74e4efdf93d998b9131ed0c2de
Mohammed Tabbakh <42747616+mohammedtabbakh@users.noreply.github.com>
2021-09-27 18:38:51 +0300

    add firebase config to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+firebaseConfig :{
+    apiKey: "AIzaSyCXLz0TwWgsRf_F_kc8U3fnvkKgrSyTuD0",
+    authDomain: "notely-694f0.firebaseapp.com",
+    projectId: "notely-694f0",
+    storageBucket: "notely-694f0.appspot.com",
+    messagingSenderId: "87117305022",
+    appId: "1:87117305022:web:2499bc8eabf04bcc8afaef",
+    measurementId: "G-CB4JR2X7XW"
 };
